b252b0e3b603fdc139ec2dbb6354c21f3b8f7eb7
unknown <maarek.vettik@ut.ee>
2025-11-20 00:25:36 +0200

    Väiksed muutused

--- scripts/hamburger.js
+++ scripts/hamburger.js
@@ -2,3 +2,5 @@ function toggleNav() {
             var navElement = document.getElementById("lingid");
             navElement.classList.toggle("responsive");
         }
+
+// https://www.w3schools.com/jsref/prop_element_classlist.asp
